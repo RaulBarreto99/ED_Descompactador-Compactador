a432f45e633d601a9fdb3bc61b5e348f7ad0e009
Raul <Raul@192.168.0.139>
2020-04-29 23:16:00 -0300

    VAI TNC NMRL

--- ProjetoEstruturaDados/src/projetoestruturadados/ListaEncadeada.java
+++ ProjetoEstruturaDados/src/projetoestruturadados/ListaEncadeada.java
@@ -5,27 +5,23 @@
  */
 package projetoestruturadados;
 
-import java.util.Scanner;
 
 /**
  *
- * @author Lucas
+ * @author RaulBarreto
  */
 public class ListaEncadeada {
-    
+
     private No ini;
 
-    //Cria lista vazia
     public ListaEncadeada() {
         this.ini = null;
     }
 
-    //Verifica se a lista está vazia
     public boolean vazia() {
         return ini == null;
     }
 
-    //Inserir no início da lista
     public void insereInicio(String elemento) {
         No novo = new No(elemento, ini);
         ini = novo;
@@ -43,79 +39,32 @@ public class ListaEncadeada {
         return strLista;
     }
 
-    public No getIni() {
-        return ini;
-    }
-
-    public void insereFinal(String elemento) {
-        No novo = new No(elemento, null);
+    public int buscaLinearIt(String x) {
         No temp = ini;
-
-        if (temp == null) { //Lista vazia
-            ini = novo;
-        } else {
-            while (temp.getProx() != null) {
-                temp = temp.getProx();
-            }
-            temp.setProx(novo);
-        }
-    }
-
-    public void insereFinalR(String elemento) {
-        insereFinalR(ini, elemento);
-    }
-
-    public void insereFinalR(No temp, String elemento) {
-        //Lista vazia
-        if (temp == null) {
-            No novo = new No(elemento, ini);
-            ini = novo;
-        } else {
-            if (temp.getProx() == null) { //Último No
-                No novo = new No(elemento, null);
-                temp.setProx(novo);
-            } else {
-                insereFinalR(temp.getProx(), elemento);
-            }
-        }
-    }
-
-    public boolean buscaLinearIt(String x) {
-        No temp = ini;
+        int cont = 0;
 
         while (temp != null) {
-            if (temp.getElemento() == x) {//Achou
-                return true;
+            if (temp.getElemento().equals(x)) {
+                cont++;
+                return cont;
+            }
+            cont++;
+            temp = temp.getProx();
+        }
+        return -1;
+    }
+    
+     public void remove(String elemento){
+        No temp = ini;
+        if(ini.getElemento().equals(elemento)){
+        ini = temp.getProx();
+        }else{
+        while(temp.getProx() != null){
+            if(temp.getProx().getElemento().equals(elemento)){
+                temp.setProx(temp.getProx().getProx());
             }
             temp = temp.getProx();
         }
-        return false;//Não achou
-    }
-
-    public No buscaLinearIt2(String x) {
-        No temp = ini;
-
-        while (temp != null) {
-            if (temp.getElemento() == x) {//Achou
-                return temp;
-            }
-            temp = temp.getProx();
         }
-        return null;//Não achou
-    }
-
-    public void inverterLista() {
-        No temp = ini;
-        No anterior = null;
-        No posterior = null;
-        while (temp.getProx() != null) {
-            posterior = anterior;
-            anterior = temp;
-            temp = temp.getProx();
-            anterior.setProx(posterior);
-        }
-        ini = temp;
-        temp.setProx(anterior);
     }
 }
-
--- ProjetoEstruturaDados/src/projetoestruturadados/Projeto.java
+++ ProjetoEstruturaDados/src/projetoestruturadados/Projeto.java
@@ -6,72 +6,103 @@
 package projetoestruturadados;
 
 import java.io.*;
+import static java.lang.Character.isLetter;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 /**
  *
  * @author Lucas
  */
 public class Projeto {
-
-    static ArrayList<String> lista = new ArrayList<>();
+    
+    static ListaEncadeada listaEncadeada = new ListaEncadeada();
 
     public static void main(String[] args) throws FileNotFoundException, IOException {
+
         String arquivo = "Dale.txt";
         String linha = "";
-        int cont = 0;
-        try {
-
-            FileReader fileReader = new FileReader(arquivo);
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
-
-            while ((linha = bufferedReader.readLine()) != null) {
+        
+        
+        FileReader fileReader = new FileReader(arquivo);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+         String arquivoDeSaida = "saida.txt";
+            
+                FileWriter fileWriter = new FileWriter(arquivoDeSaida);
+                BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+        while ((linha = bufferedReader.readLine()) != null) {
+         ArrayList<String> lista = new ArrayList<>();
+            if (!linha.equals("0")) {
                 lista.add(linha);
-            }
-
-        } catch (IOException ex) {
-            System.out.println("Erro de escrita em '" + "'");
-        }
-        
-        ArrayList<String> listaPalavra = new ArrayList<>();
-        
-        for (int i = 0; i < lista.size(); i++) {
-            //System.out.println(lista.get(i));
-            String[] array = lista.get(i).split(" ");
-            
-            for (int j = 0; j < array.length; j++) {
-                listaPalavra.add(array[j]);
-            }
-        }
-        ArrayList<String> fim = new ArrayList<>();
-        ListaEncadeada listaEncadeada = new ListaEncadeada();
-        
-        for (int i = 0; i < listaPalavra.size(); i++) {
-            
-            if(listaEncadeada.buscaLinearIt(listaPalavra.get(i))){
-                
+            }else{
+                bufferedReader.close();
             }
             
+
+            ArrayList<Character> listaChar = new ArrayList<>();
+
+            for (int i = 0; i < lista.size(); i++) {
+                char[] array = lista.get(i).toCharArray();
+
+                for (int j = 0; j < array.length; j++) {
+                    listaChar.add(array[j]);
+                }
+            }
+                        System.out.println("pika: "+listaChar);
+            ArrayList<String> listaPalavra = new ArrayList<>();
+            ArrayList<String> listaFim = new ArrayList<>();
+            
+
+            String palavra = "";
+            for (int i = 0; i < listaChar.size(); i++) {
+                if (listaChar.get(i).equals(' ')) {
+                    if (!palavra.equals("")) {
+                        listaPalavra.add(palavra);
+                    }
+                    palavra = "";
+                } else if (!isLetter(listaChar.get(i))) {
+                    if (!palavra.equals("")) {
+                        listaPalavra.add(palavra);
+                    }
+                    palavra = "";
+                    listaPalavra.add(listaChar.get(i).toString());
+                } else {
+                    palavra += listaChar.get(i);
+                }
+            }
+            System.out.println("lista em palavras: " + listaPalavra);
+
+            for (int i = 0; i < listaPalavra.size(); i++) {
+                if (listaPalavra.get(i).matches("[^a-zA-Z0-9]")) {
+                    listaFim.add(listaPalavra.get(i));
+                } else if (listaEncadeada.buscaLinearIt(listaPalavra.get(i)) != -1) {
+                    int posicao = listaEncadeada.buscaLinearIt(listaPalavra.get(i));
+                    listaFim.add(String.valueOf(posicao));
+                    listaEncadeada.remove(listaPalavra.get(i));
+                    listaEncadeada.insereInicio(listaPalavra.get(i));
+                } else {
+                    listaFim.add(listaPalavra.get(i));
+                    listaEncadeada.insereInicio(listaPalavra.get(i));
+                }
+
+            }
+            System.out.println("lista compactada: " + listaFim);
+            System.out.println("lista encadeada qua salva as ocorrencias de cada palavra: " + listaEncadeada);
+
+            //Saida
+            
+
+                for (int i = 0; i < listaFim.size(); i++) {
+                    bufferedWriter.write(listaFim.get(i));
+                    
+                    if(i < listaFim.size()-1){
+                    if(!listaFim.get(i+1).equals(",") && !listaFim.get(i+1).equals(".") && !listaFim.get(i+1).equals("-") && !listaFim.get(i+1).equals("'")){
+                    bufferedWriter.write(" ");
+                    }
+                }
+                }
+                bufferedWriter.newLine();
+                // feche o arquivo
         }
-
-        String arquivoDeSaida = "saida.txt";
-
-        try {
-
-            FileWriter fileWriter = new FileWriter(arquivoDeSaida);
-            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-
-            bufferedWriter.write("pib da regiao X = $$$$");
-            bufferedWriter.newLine();
-            bufferedWriter.write("pib da regiao Y = $$$$");
-
-            // feche o arquivo
-            bufferedWriter.close();
-        } catch (IOException ex) {
-            System.out.println("Erro de escrita em '" + arquivoDeSaida + "'");
-        }
-
+        bufferedWriter.close();
     }
-
 }
